0050011bdf76a631a521134f5a5fa6d6bd30bc1a
coyte <niels@teekens.info>
2022-06-23 23:01:32 +0200

    initial

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#Set IP address
+rm -f /etc/systemd/network/* #delete existing network config
+
+cat > /etc/systemd/network/static.network << EOF
+[Match]
+Name=eth0
+[Network]
+Domains=$FQDN
+Address=$IPADDR
+Gateway=$GATEWAY
+DNS=$DNS
+EOF
+
+systemctl restart systemd-networkd
+
+
+#Set hostname
+hostnamectl set-hostname ${FQDN%%.*}
+
+# Add alias
+cat > /etc/profile.d/alias.sh << EOF
+#!/bin/bash
+alias ll='ls -al'
+EOF
+
+#Enable ssh root login
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+systemctl restart sshd
+
+#Update packages
+tdnf update
+tdnf -y install 
+
+#Add rclone for public storage mount
+curl https://rclone.org/install.sh | sudo bash
+
---
+++ env-vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#example file for environment variable needed for the deployment script
+export OLDPW=changeit #default password of the Photon OVA
+export NEWPW=mypasswd #new root password
+export IPADDR=10.10.10.10/24 #IP address in CIDR notation
+export GATEWAY=10.10.10.1 # Gateway IP address
+export DNS=10.10.10.1 # IP address of the DNS server
